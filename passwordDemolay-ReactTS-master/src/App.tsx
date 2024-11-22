1220d0700235f0a86bcacf5e28029e46284a38a6
vinirteuber <vinireisteur@gmail.com>
2024-11-21 22:41:04 -0300

    alteracoes

--- passwordDemolay-ReactTS-master/src/App.tsx
+++ passwordDemolay-ReactTS-master/src/App.tsx
@@ -3,8 +3,6 @@ import PasswordForm from "./components/PasswordForm";
 import PasswordList from "./components/PasswordList";
 import "./index.css";
 import CarouselGold from "./components/CarouselGold";
-import CarouselBronze from "./components/CarouselBronze";
-import CarouselApoiador from "./components/CarouselApoiador";
 import soundFile from "./assets/effect.mp3";
 
 interface Password {
@@ -16,7 +14,6 @@ interface Password {
 const App: React.FC = () => {
   const [passwords, setPasswords] = useState<Password[]>([]);
   const [currentPassword, setCurrentPassword] = useState<Password | null>(null);
-  const [currentCarousel, setCurrentCarousel] = useState<string>("gold");
 
   useEffect(() => {
     const storedPasswords = localStorage.getItem("passwords");
@@ -66,35 +63,9 @@ const App: React.FC = () => {
   const preparacaoPasswords = passwords.filter((password) => !password.chamada);
   const chamadaPasswords = passwords.filter((password) => password.chamada);
 
-  useEffect(() => {
-    const carouselTimeout = setTimeout(() => {
-      if (currentCarousel === "gold") {
-        setCurrentCarousel("bronze");
-      } else if (currentCarousel === "bronze") {
-        setCurrentCarousel("apoiador");
-      } else {
-        setCurrentCarousel("gold");
-      }
-    }, getCarouselTimeout(currentCarousel));
-
-    return () => clearTimeout(carouselTimeout);
-  }, [currentCarousel]);
-
-  const getCarouselTimeout = (carousel: string): number => {
-    if (carousel === "gold") {
-      return 5 * 60 * 1000; // 5 minutos em milissegundos
-    } else if (carousel === "bronze") {
-      return 3 * 60 * 1000; // 3 minutos em milissegundos
-    } else {
-      return 1 * 60 * 1000; // 1 minuto em milissegundos
-    }
-  };
-
   return (
     <>
-      {currentCarousel === "gold" && <CarouselGold />}
-      {currentCarousel === "bronze" && <CarouselBronze />}
-      {currentCarousel === "apoiador" && <CarouselApoiador />}
+      <CarouselGold />
       <div className="container">
         <div className="left-side">
           <div className="password-container">
@@ -108,7 +79,7 @@ const App: React.FC = () => {
         </div>
         <div className="right-side">
           <div className="preparacao-passwords">
-            <h2>Senhas que já foram chamadas</h2>
+            <h2>Senhas anteriores</h2>
             <PasswordList
               passwords={chamadaPasswords.slice(0, 4)}
               onClickPassword={handleClickPassword}
--- passwordDemolay-ReactTS-master/src/components/CarouselGold.tsx
+++ passwordDemolay-ReactTS-master/src/components/CarouselGold.tsx
@@ -1,13 +1,71 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import MeO from "../assets/logo-mosistemas-seo.png";
-import Esquadro from "../assets/esquadroatelie.jpeg";
-import Lufiego from "../assets/lufiegoatualizada.jpeg";
+import logo2 from "../assets/2.png";
+import logo3 from "../assets/3.png";
+import logo4 from "../assets/4.png";
+import logo5 from "../assets/5.png";
+import logo6 from "../assets/6.png";
+import logo7 from "../assets/7.png";
+import logo8 from "../assets/8.png";
+import logo9 from "../assets/9.png";
+import logo10 from "../assets/10.png";
+import logo11 from "../assets/11.png";
+import logo12 from "../assets/12.png";
+import logo13 from "../assets/13.png";
+import logo14 from "../assets/14.png";
+import logo15 from "../assets/15.png";
+import logo16 from "../assets/16.png";
+import logo17 from "../assets/17.png";
+import logo18 from "../assets/18.png";
+import logo19 from "../assets/19.png";
+import logo20 from "../assets/20.png";
+import logo21 from "../assets/21.png";
+import logo22 from "../assets/22.png";
+import logo23 from "../assets/23.png";
+import logo24 from "../assets/24.png";
+import logo25 from "../assets/25.png";
+import logo26 from "../assets/26.png";
+import logo27 from "../assets/27.png";
+import logo28 from "../assets/28.png";
+import logo29 from "../assets/29.png";
+import logo30 from "../assets/30.png";
+import logo31 from "../assets/31.png";
+import logo32 from "../assets/32.png";
+import logo33 from "../assets/33.png";
+import logo34 from "../assets/34.png";
+import logo35 from "../assets/35.png";
+import logo36 from "../assets/36.png";
+import logo37 from "../assets/37.png";
+import logo38 from "../assets/38.png";
+import logo39 from "../assets/39.png";
+import logo40 from "../assets/40.png";
+import logo41 from "../assets/41.png";
+import logo42 from "../assets/42.png";
+import logo43 from "../assets/43.png";
+import logo44 from "../assets/44.png";
+import logo45 from "../assets/45.png";
+import logo46 from "../assets/46.png";
+import logo47 from "../assets/47.png";
+import logo48 from "../assets/48.png";
+import logo49 from "../assets/49.png";
+import logo50 from "../assets/50.png";
+import logo51 from "../assets/51.png";
+import logo52 from "../assets/52.png";
+import logo53 from "../assets/53.png";
+import logo54 from "../assets/54.png";
+import logo55 from "../assets/55.png";
+import logo56 from "../assets/56.png";
+import logo57 from "../assets/57.png";
+import logo58 from "../assets/58.png";
+import logo59 from "../assets/59.png";
+import logo60 from "../assets/60.png";
+import logo61 from "../assets/61.png";
+import logo62 from "../assets/62.png";
 
 const CarouselGold = () => {
   const settings = {
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
@@ -19,13 +77,187 @@ const CarouselGold = () => {
     <div className="all">
       <Slider {...settings}>
         <div>
-          <img className="ouro" src={MeO} alt="patrocinio" />
+          <img className="ouro" src={logo2} alt="patrocinio" />
         </div>
         <div>
-          <img className="ouro" src={Esquadro} alt="patrocinio" />
+          <img className="ouro" src={logo3} alt="patrocinio" />
         </div>
         <div>
-          <img className="ouro" src={Lufiego} alt="patrocinio" />
+          <img className="ouro" src={logo4} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo5} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo6} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo7} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo8} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo9} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo10} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo11} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo12} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo13} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo14} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo15} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo16} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo17} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo18} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo19} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo20} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo21} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo22} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo23} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo24} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo25} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo26} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo27} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo28} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo29} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo30} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo31} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo32} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo33} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo34} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo35} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo36} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo37} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo38} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo39} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo40} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo41} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo42} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo43} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo44} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo45} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo46} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo47} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo48} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo49} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo50} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo51} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo52} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo53} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo54} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo55} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo56} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo57} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo58} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo59} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo60} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo61} alt="patrocinio" />
+        </div>
+        <div>
+          <img className="ouro" src={logo62} alt="patrocinio" />
         </div>
       </Slider>
     </div>
